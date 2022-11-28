be22e82a8947fe442744d04a2a8d60640dcbd6a9
ewaite214 <119267342+ewaite214@users.noreply.github.com>
2022-11-28 14:36:18 +1100

    1 file modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,20 @@ import {MatSidenav} from '@angular/material/sidenav';
 import { MyService } from './services/my-service.service';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // ViewChild is a method that allows you 
-  // to get a html element from your html page
-@ViewChild('snav') snav: MatSidenav;
-
-constructor(
-  private ms: MyService
-  ) {   }
-
-  close() {
-      this.snav.close();
-  }
+    // ViewChild is a method that allows you 
+    // to get a html element from your html page
+ @ViewChild('snav') snav: MatSidenav;
+ 
+ constructor(
+    private ms: MyService
+    ) {   }
+ 
+    close() {
+        this.snav.close();
+    }
 }
